02580617b0c6bf75070f9e0241c0f51fb3dc5db8
Bruno Giugno <brunogiugno@gmail.com>
2025-11-25 21:07:49 -0300

    estructura nueva

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,95 +362,6 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
-export interface ApiCategoryCategory extends Schema.CollectionType {
-  collectionName: 'categories';
-  info: {
-    singularName: 'category';
-    pluralName: 'categories';
-    displayName: 'Category';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    categoryName: Attribute.String;
-    slug: Attribute.UID<'api::category.category', 'categoryName'>;
-    mainimage: Attribute.Media;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'api::category.category',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'api::category.category',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
-export interface ApiProductProduct extends Schema.CollectionType {
-  collectionName: 'products';
-  info: {
-    singularName: 'product';
-    pluralName: 'products';
-    displayName: 'Product';
-    description: '';
-  };
-  options: {
-    draftAndPublish: true;
-  };
-  attributes: {
-    productName: Attribute.String;
-    slug: Attribute.UID<'api::product.product', 'productName'>;
-    description: Attribute.Text;
-    images: Attribute.Media;
-    active: Attribute.Boolean & Attribute.DefaultTo<true>;
-    price: Attribute.Integer;
-    origin: Attribute.Enumeration<['Argentina', 'Uruguay', 'Brasil']>;
-    tipoProducto: Attribute.Enumeration<
-      [
-        'shampoo',
-        'acondicionador',
-        'bano_de_crema',
-        'tonico_capilar',
-        'tinturas',
-        'accesorios',
-        'decolorantes'
-      ]
-    >;
-    isFeatured: Attribute.Boolean;
-    tipoCabello: Attribute.Enumeration<
-      ['todo_tipo\t', 'graso\t', 'seco\t', 'te\u00F1ido\t', 'da\u00F1ado']
-    >;
-    category: Attribute.Relation<
-      'api::product.product',
-      'oneToOne',
-      'api::category.category'
-    >;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    publishedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'api::product.product',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'api::product.product',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -872,6 +783,105 @@ export interface PluginI18NLocale extends Schema.CollectionType {
   };
 }
 
+export interface ApiCategoryCategory extends Schema.CollectionType {
+  collectionName: 'categories';
+  info: {
+    singularName: 'category';
+    pluralName: 'categories';
+    displayName: 'Category';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    categoryName: Attribute.String;
+    slug: Attribute.UID<'api::category.category', 'categoryName'>;
+    mainimage: Attribute.Media;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::category.category',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::category.category',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
+export interface ApiProductProduct extends Schema.CollectionType {
+  collectionName: 'products';
+  info: {
+    singularName: 'product';
+    pluralName: 'products';
+    displayName: 'Product';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    productName: Attribute.String;
+    slug: Attribute.UID<'api::product.product', 'productName'>;
+    description: Attribute.Text;
+    images: Attribute.Media;
+    active: Attribute.Boolean & Attribute.DefaultTo<true>;
+    price: Attribute.Integer;
+    origin: Attribute.Enumeration<['Mendoza', 'San Luis', 'San Juan']>;
+    tipoProducto: Attribute.Enumeration<
+      [
+        'Shampoo',
+        'Acondicionador',
+        'Reparador de Puntas',
+        'Ampollas',
+        'Ba\u00F1o De Crema',
+        'T\u00F3nico Capilar',
+        'Finalizador',
+        'Tinturas',
+        'Accesorios',
+        'Decolorantes'
+      ]
+    >;
+    isFeatured: Attribute.Boolean;
+    tipoCabello: Attribute.Enumeration<
+      [
+        'Todo Tipo\t',
+        'Para Cabellos Grasos\t',
+        'Para Cabellos Secos\t',
+        'Para Cabellos Te\u00F1idos\t',
+        'Para Cabellos Da\u00F1ados'
+      ]
+    >;
+    category: Attribute.Relation<
+      'api::product.product',
+      'oneToOne',
+      'api::category.category'
+    >;
+    marca: Attribute.Enumeration<['HAN', 'THERAPHY', 'PROTENAT', 'M&D']>;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::product.product',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::product.product',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
@@ -882,8 +892,6 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
-      'api::category.category': ApiCategoryCategory;
-      'api::product.product': ApiProductProduct;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
@@ -892,6 +900,8 @@ declare module '@strapi/types' {
       'plugin::users-permissions.role': PluginUsersPermissionsRole;
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
       'plugin::i18n.locale': PluginI18NLocale;
+      'api::category.category': ApiCategoryCategory;
+      'api::product.product': ApiProductProduct;
     }
   }
 }
